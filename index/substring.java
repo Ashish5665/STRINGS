83a83c115aa9082e9a110617e167713b89ceb367
Ashish5665 <kattaashishkumarpsycho@gmail.com>
2023-02-06 14:45:11 +0530

    substring

---
+++ index/substring.java
@@ -0,0 +1,8 @@
+package index;
+
+public class substring{
+    public static void main(String args[]){
+        String s1="javatpoint";
+        System.out.println(s1.substring(2,4));
+        System.out.println(s1.substring(2));
+    }}
