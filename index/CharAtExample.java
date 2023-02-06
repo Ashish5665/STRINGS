26b7233e53c0458aaf7952bf622ee25e74e29d9b
Ashish5665 <kattaashishkumarpsycho@gmail.com>
2023-02-06 14:57:22 +0530

    CharAtExample

--- index/CharAtExample.java
+++ index/CharAtExample.java
@@ -3,6 +3,6 @@ package index;
 public class CharAtExample{
     public static void main(String args[]){
         String name="javatpoint";
-        char ch=name.charAt(4);//returns the char value at the 4th index
+        char ch=name.charAt(4);
         System.out.println(ch);
     }}
